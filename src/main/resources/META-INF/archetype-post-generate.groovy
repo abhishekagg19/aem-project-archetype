d039a2b1542ebc0cfd3883f429e3e6016d6c8ad5
Abhishek <abaggarw@adobe.com>
2019-07-30 09:25:48 +0530

    Dispatcher config module

--- src/main/resources/META-INF/archetype-post-generate.groovy
+++ src/main/resources/META-INF/archetype-post-generate.groovy
@@ -2,7 +2,12 @@ def optionIncludeErrorHandler = request.getProperties().get("optionIncludeErrorH
 
 def rootDir = new File(request.getOutputDirectory() + "/" + request.getArtifactId())
 def uiAppsPackage = new File(rootDir, "ui.apps")
+def dispatcherConfig = new File(rootDir, "dispatcher")
 
 if (optionIncludeErrorHandler == "n") {
     assert new File(uiAppsPackage, "src/main/content/jcr_root/apps/sling").deleteDir()
+}
+
+if ( generateDispatcherConfig == "none" || generateDispatcherConfig == "n"  ) {
+    assert dispatcherConfig.deleteDir()
 }
